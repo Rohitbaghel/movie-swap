3b2584503593a604c825d92bca655e80e69888d8
Rohit Baghel <87422193+Rohitbaghel@users.noreply.github.com>
2023-01-23 12:56:23 +0530

    movie swap

---
+++ src/firebase/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: "movie-swap-2ea99.firebaseapp.com",
+  projectId: "movie-swap-2ea99",
+  storageBucket: "movie-swap-2ea99.appspot.com",
+  messagingSenderId: "849605581227",
+  appId: "1:849605581227:web:912febaa8c9b91cd6f5aee",
+  measurementId: "G-Z3QR1X7LN4",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export const moviesRef = collection(db, "movies");
+export const reviewsRef = collection(db, "reviews");
+export const usersRef = collection(db, "users");
+
+export default app;
